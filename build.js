cfca71cb64fdb64d2790a7b80f1b265875bcae36
omarosman107 <omarosman107@gmail.com>
2018-11-20 15:40:18 -0500

    edit

--- build.js
+++ build.js
@@ -1199,7 +1199,11 @@ if (json.length - tempLS["?" + json.href] < json.end) {
       //          <span class="episode-gradient"></span>
         //  document.getElementById('watching').innerHTML += '<div tabindex="1" class="wtc '+json[i].href+'"><a onclick="loadPlayer(this)" href="player.html?'+json[i].href+'" ><img width="100%" src="'+json[i].img+'"><div id="projpar" class="w3-progress-container" style=""><div id="progress" class="w3-progressbar" style="width: '+perc+'%;"><\/div><\/div><br> <span>'+json[i].show+'<\/span><\/a><\/div>'
        console.log(Math.round((json.length - tempLS["?" + json.href]) / 60), 'mins left.')
- var Timeleft = Math.round((json.length - tempLS["?" + json.href]) / 60) +  ' mins left'
+        var Timeleft = Math.round((json.length - tempLS["?" + json.href]) / 60) +  ' mins left'
+
+       if(!tempLS["?" + json.href]){
+       	Timeleft = ''
+       }
  if(Math.round((json.length - tempLS["?" + json.href]) / 60) == 0){
  	Timeleft = 'almost done'
  }
